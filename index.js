17ce6c41c8e7b43f1e9f12fe66e4e65ec9f7a487
olson3r <scootles.olson@gmail.com>
2015-03-11 18:18:16 -0500

    Use dust.onLoad to compile templates

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var dust
+var fs = require('fs')
+var Path = require('path')
 
 try {
   dust = require('dustjs-linkedin')
@@ -15,6 +17,14 @@ if (!dust) throw new Error('"dustjs-linkedin" or "dust" module not found')
 module.exports = {
   module: {
     compile: function(template, options, callback) {
+      var fileExtension = options.defaultExtension || 'dust'
+      dust.onLoad = function(name, callback) {
+          fs.readFile(Path.join(options.basedir, name + '.' + fileExtension), function(err, data) {
+          if (err)
+            throw err
+          callback(err, data.toString())
+        })
+      }
       var compiled = dust.compileFn(template, options && options.name)
       process.nextTick(function() {
         callback(null, function(context, options, callback) {
